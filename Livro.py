92506c4e038c439605b25cffd7b7279062b3c24b
jp_almeida <joao.pedro.jpda@gmail.com>
2024-09-15 12:50:16 -0300

    Adicionando valores default dos atributos

--- Livro.py
+++ Livro.py
@@ -2,12 +2,9 @@ import itertools
 
 class Livro:
   id_iter = itertools.count()
-  def __init__(self, titulo, genero, ano, autor):
+  def __init__(self, titulo, genero = 'Geral', ano = '2024', autor_id = None):
     self.id = next(self.id_iter)
     self.titulo = titulo 
     self.genero = genero
     self.ano = ano
-    self.autor = autor
-  
-  def display(self):
-    print(self.id)
+    self.autor_id = autor_id
